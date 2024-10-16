da757ee7925ba6341b388553c6fdf1da59de4940
patyschmid <patriciaschmid13@gmail.com>
2024-10-15 22:00:24 -0300

    commit final

---
+++ desafio.js
@@ -0,0 +1,26 @@
+// Variáveis
+let nome = "Patrícia"; // Nome do herói
+let experiencia = 20000; // Quantidade de XP do herói
+let nivel; // Variável para armazenar o nível
+
+// Estrutura de decisão para determinar o nível com base na experiência
+if (experiencia < 1000) {
+  nivel = "Ferro";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+  nivel = "Bronze";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+  nivel = "Prata";
+} else if (experiencia >= 5001 && experiencia <= 7000) {
+  nivel = "Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+  nivel = "Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+  nivel = "Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
